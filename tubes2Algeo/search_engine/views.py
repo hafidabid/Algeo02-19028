dc3e6b4914ebf4aa7cc1c6e7734485275b500120
hafidabi <abi.daniswara@gmail.com>
2020-11-11 21:25:09 +0700

    django

--- tubes2Algeo/search_engine/views.py
+++ tubes2Algeo/search_engine/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+
+
+def cobacek(request):
+    text = """<h1>welcome to my app !</h1>"""
+    return render(request,'index.html',{})
--- tubes2Algeo/tubes2Algeo/urls.py
+++ tubes2Algeo/tubes2Algeo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from search_engine import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'',views.cobacek,{})
 ]
